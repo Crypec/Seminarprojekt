7056a34e4789b3c8f9a1eefc82b53e57a4ebb0f1
Simon Kunz <simon.kunz21@gmail.com>
2019-09-27 14:23:32 +0200

    refactor konrad and make language spec simpler

--- src/main/java/util/ASTNode.java
+++ src/main/java/util/ASTNode.java
@@ -1,29 +1,13 @@
 package konrad.util;
 
-public class ASTNode {
+public abstract class ASTNode {
 
     private ASTNode left;
     private ASTNode right;
-
-    private NodeType type;
-    public Object value;
-
-    public ASTNode (NodeType type, Object value,  ASTNode right, ASTNode left) {
-	this.right = right;
-	this.left = left;
-	this.value = value;
-	this.type = type;
-    }
-
-    public ASTNode getLeft() {
-	return this.left;
-    }
     
-    public ASTNode getRight() {
-	return this.right;
+    public static class FunctionDecl extends ASTNode {
+	
     }
 
-    public NodeType getNodeType() {
-	return this.type;
-    }
+    
 }
