141f3a516d0cf207160ec5201b7971908e4750ef
Simon Kunz <simon.kunz21@gmail.com>
2019-09-28 21:24:33 +0200

    Add constructor for token metadata

--- src/main/java/util/MetaData.java
+++ src/main/java/util/MetaData.java
@@ -3,7 +3,14 @@ package konrad.util;
 // @PERF(Simon): this is just a workaround until I have  a better idea to do error handling
 // it is not efficient to save all the metadata like filenames in every token of a sourcefile
 public class MetaData {
-  public String filename;
-  public int line;
-  public int colum;
+    public String filename;
+    public int line;
+    public int colum;
+
+    public MetaData(String filename, int line, int colum) {
+	this.filename = filename;
+	this.line = line;
+	this.colum = colum;
+    }
+
 }
