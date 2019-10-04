349f0ede49f74617a258de7e02e8ffe50e0fa39e
Simon Kunz <simon.kunz21@gmail.com>
2019-10-04 22:17:19 +0200

    move lexer to tokenBuilder

--- src/main/java/konrad/Lexer.java
+++ src/main/java/konrad/Lexer.java
@@ -1,82 +1,67 @@
 package konrad;
 
-import java.util.ArrayList;
+import java.util.*;
 import konrad.util.*;
 
 public class Lexer {
 
-    // NOTE(Simon): this is a brutally simple version of the lexer, there is a
-    // great chance we need to rewrite this NOTE(Simon): to focus on Error
-    // handling... (making it compatible with taking a sourcefile)
-    // FIX(Simon): this function is a pure mess, I should really fix it some day
-    // => make it compatible with taking a raw sourceFile
+    //TODO(Simon): refactor to Object?
+    //FIXME(Simon): start and End positon don't quite work, should be getting fixed
     public static ArrayList<Token> tokenize(StringIterator it) {
 
 	var tokenStream = new ArrayList<konrad.util.Token>();
 	var sb = new StringBuilder();
 
+
+	//NOTE(Simon): These are trivially to compute if we ever encounter an error. Maybe we should only compute these if we have to do it for error handling?
+	//NOTE(Simon):
 	int startPos = 0;
 	int endPos = 0;
 
 	while (it.hasNext()) {
+
+	    endPos += 1;
 	    char c = it.next();
+
 	    if (c == ' ' || c == 9) { // check for tab (ASCII code 9)
 		if (sb.length() != 0) {
-		    var token = new Token.Builder()
-			.filename(it.getFilename())
-			.line(it.getLine())
-			.position(startPos, endPos)
-			.lexeme(sb.toString())
-			.build();
-		    tokenStream.add(token);
+		    tokenStream.add(new Token.Builder()
+				    .filename(it.getFilename())
+				    .line(it.getLine())
+				    .position(startPos, endPos)
+				    .lexeme(sb.toString())
+				    .build());
 		    startPos = endPos;
-		    endPos = 0;
 		    sb.setLength(0); // clear StringBuilder
 		} else {
-		    endPos++;
 		    continue; // if StringBuilder is empty we can just skip the whitespace
 		}
 	    } else if (c == '"') {
+		startPos = endPos;
 		tokenStream.add(getStringLiteral(it));
 	    } else if (Character.isDigit(c)) {
-		var token = getNumLiteral(it, c);
-		tokenStream.add(token);
+		startPos = endPos;
+		tokenStream.add(Lexer.getNumLiteral(it, c));
 	    } else if (c == '/' && it.peek() == '/') {
-		// looks like we encountered a comment and can skip the rest of the line
-		return tokenStream;
+		return tokenStream; // looks like we encountered a comment and can skip the rest of the line
 	    } else if (Token.isSingleCharToken(c)) {
 		if (sb.length() != 0) {
-		    var token = new Token.Builder()
-			.filename(it.getFilename())
-			.line(it.getLine())
-			.position(startPos, endPos)
-			.lexeme(sb.toString())
-			.build();
-		    tokenStream.add(token);
-
+		    tokenStream.add(new Token.Builder()
+				    .filename(it.getFilename())
+				    .line(it.getLine())
+				    .position(startPos, endPos)
+				    .lexeme(sb.toString())
+				    .build());
 		    startPos = endPos;
-		    endPos = 0;
 		    sb.setLength(0);
-
-		    token = new Token.Builder()
-			.filename(it.getFilename())
-			.line(it.getLine())
-			.position(startPos, endPos)
-			.lexeme(Character.toString(c))
-			.build();
-		    tokenStream.add(token);
-		    startPos = endPos;
-		    endPos = 0;
 		} else {
-		    var token = new Token.Builder()
-			.filename(it.getFilename())
-			.line(it.getLine())
-			.position(startPos, endPos)
-			.lexeme(Character.toString(c))
-			.build();
+		    tokenStream.add(new Token.Builder()
+				    .filename(it.getFilename())
+				    .line(it.getLine())
+				    .position(startPos, endPos)
+				    .lexeme(Character.toString(c))
+				    .build());
 		    startPos = endPos;
-		    endPos = 0;
-		    tokenStream.add(token);
 		}
 	    } else {
 		sb.append(c);
@@ -106,32 +91,39 @@ public class Lexer {
 		sb.append(c);
 	    }
 	}
-	// FIX(Simon): this can fail if string is initalized as empty
-	/*
-	  a : Text = ""; //this would fail in the current implementation
+	// FIXME(Simon): this can fail if string is initalized as empty
+	/* 
+	   a : Text = ""; //this would fail in the current implementation
 	*/
-	var token = new Token.Builder()
+	return new Token.Builder()
 	    .lexeme(sb.toString())
 	    .filename(it.getFilename())
 	    .line(it.getLine())
 	    .build();
-	return token;
     }
+
+    public static boolean emitSemicolon(List<Token> tokenStream) {
+	boolean emitEOE = false;
+	// for (Token t : tokenStream) {
+	//     emitEOE |= endOFExprTokenNeeded(t);
+	// }
+	return emitEOE;
+    }
+    
 
     /*
       Right now numeric value can look like this:
 
-      foo: Zahl = 123_23
+      foo: Zahl = 12'323,23
 
       we only skip underscores, internally every number gets represented as a
       64bit float we can think about using some other representation for numbers
       in the future maybe DEC64 would be nice: http://www.dec64.com/
     */
     public static Token getNumLiteral(StringIterator it, Character firstDigit) {
-	var sb = new StringBuilder(firstDigit);
+	var sb = new StringBuilder();
 	sb.append(firstDigit);
 
-	outer:
 	while (it.hasNext()) {
 	    var c = it.next();
 
@@ -148,18 +140,17 @@ public class Lexer {
 	    break;
 	}
 
-	var token = new Token.Builder()
+	return new Token.Builder()
 	    .lexeme(sb.toString())
 	    .filename(it.getFilename())
 	    .line(it.getLine())
 	    .build();
-	return token;
     }
 
     public static double parseNum(String str) {
 	return Double.parseDouble(str);
     }
-
+    
     public static ArrayList<Token> tokenize(SourceFile sf) {
 	var tokenStream = new ArrayList<Token>();
 	while (sf.hasNext()) {
