297c35081350b66a8cd4ac6100f18ed434798b93
Simon Kunz <simon.kunz21@gmail.com>
2019-10-04 14:05:40 +0200

    add Typer class

---
+++ src/main/java/konrad/Typer.java
@@ -0,0 +1,11 @@
+package konrad;
+
+import konrad.util.*;
+
+public class Typer {
+
+    //TODO(Simon): Perform typechecking :D
+    public static boolean ASTisValid(Stmt AST) {
+	return true;
+    }
+}
