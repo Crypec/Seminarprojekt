dc3f8952c0a42a32a1600cad202ee0ff3bc222c6
Simon Kunz <simon.kunz21@gmail.com>
2019-12-26 00:56:39 +0100

    merging new LexerTest

--- src/test/java/core/LexerTest.java
+++ src/test/java/core/LexerTest.java
@@ -1,76 +1,336 @@
 package core;
 
+import com.google.common.collect.*;
+import java.util.*;
+import static org.junit.Assert.*;
+import org.junit.*;
+import util.*;
 
 public class LexerTest {
 
-    //  @Test
-    //  public void testTokenizeFunctionDecl() {
+    @Test
+    public void lexFunctionDecl() {
 
-    // 	String testDecl = "fun bar(x: Zahl, y: Zahl) -> Text";
+	String testcase = "fun test(x: Zahl, y: Text) -> Zahl {}";
 
-    // 	var tokenStream = Lexer.tokenize(new StringIterator(testDecl));
+	var actual = new Lexer(testcase, "LEX_TEST_FUNCTIONDECL").tokenize();
 
-    // 	var actual = new ArrayList<Token>();
-    // 	actual.add(new Token("fun"));
-    // 	actual.add(new Token("bar"));
-    // 	actual.add(new Token("("));
-    // 	actual.add(new Token("x"));
-    // 	actual.add(new Token(":"));
-    // 	actual.add(new Token("Zahl"));
-    // 	actual.add(new Token(","));
-    // 	actual.add(new Token("y"));
-    // 	actual.add(new Token(":"));
-    // 	actual.add(new Token("Zahl"));
-    // 	actual.add(new Token(")"));
-    // 	actual.add(new Token("->"));
-    // 	actual.add(new Token("Text"));
+	var expected = new ArrayList() {{
+	    add(TokenType.FUNCTION);
+	    add(TokenType.IDEN);
+	    add(TokenType.LPAREN);
+	    add(TokenType.IDEN);
+	    add(TokenType.COLON);
+	    add(TokenType.IDEN);
+	    add(TokenType.COMMA);
+	    add(TokenType.IDEN);
+	    add(TokenType.COLON);
+	    add(TokenType.IDEN);
+	    add(TokenType.RPAREN);
+	    add(TokenType.ARROW);
+	    add(TokenType.IDEN);
+	    add(TokenType.STARTBLOCK);
+	    add(TokenType.ENDBLOCK);
+	}};
+	assertListEqual(actual, expected);
+    }
 
-    // 	assertTrue(listEquals(tokenStream, actual));
-    //  }
-    //  @Test
-    //  public void testTokenizeVarDef() {
+    @Test
+    public void lexMathExpr() {
 
-    // 	String testDecl = "foo: Text = \"Hello World\"";
-    // 	var tokenStream = Lexer.tokenize(new StringIterator(testDecl));
+	String testcase = "a / (-2*3+(x))";
 
-    // 	var actual = new ArrayList<Token>();
+	var actual = new Lexer(testcase, "LEX_TEST_MATH_EXPR").tokenize();
 
-    // 	actual.add(new Token("foo"));
-    // 	actual.add(new Token(":"));
-    // 	actual.add(new Token("Text"));
-    // 	actual.add(new Token("="));
-    // 	actual.add(new Token("\"Hello World\""));
+	var expected = new ArrayList() {{
+	    add(TokenType.IDEN);
+	    add(TokenType.DIVIDE);
+	    add(TokenType.LPAREN);
+	    add(TokenType.MINUS);
+	    add(TokenType.NUMBERLITERAL);
+	    add(TokenType.MULTIPLY);
+	    add(TokenType.NUMBERLITERAL);
+	    add(TokenType.PLUS);
+	    add(TokenType.LPAREN);
+	    add(TokenType.IDEN);
+	    add(TokenType.RPAREN);
+	    add(TokenType.RPAREN);
+	}};
+	assertListEqual(actual, expected);
+    }
 
-    // 	assertTrue(listEquals(tokenStream, actual));
-    //  }
+    @Test
+    public void lexNullAssignment() {
 
-    //  @Test
-    //  public void testTokenizeWhileDecl() {
-    // 	String testDecl = "solange(x != 0) {}";
+	String testcase = "foo = #null;";
 
-    // 	var tokenStream = Lexer.tokenize(new StringIterator(testDecl));
+	var actual = new Lexer(testcase, "LEX_TEST_NULL_ASSIGNMENT").tokenize();
 
-    // 	var actual = new ArrayList<Token>();
+	var expected = new ArrayList() {{
+	    add(TokenType.IDEN);
+	    add(TokenType.EQUALSIGN);
+	    add(TokenType.NULL);
+	    add(TokenType.SEMICOLON);
+	}};
+	assertListEqual(actual, expected);
+    }
 
-    // 	actual.add(new Token("solange"));
-    // 	actual.add(new Token("("));
-    // 	actual.add(new Token("x"));
-    // 	actual.add(new Token("!="));
-    // 	actual.add(new Token("0", TokenType.NUMBERLITERAL, 0));
-    // 	actual.add(new Token(")"));
-    // 	actual.add(new Token("{"));
-    // 	actual.add(new Token("}"));
+    @Test
+    public void lexBoolExpr() {
 
-    // 	assertTrue(listEquals(tokenStream, actual));
-    //  }
+	String testcase = "((!x und y) oder (x und !y))";
 
-    //  public static boolean listEquals(ArrayList<Token> given, ArrayList<Token> wanted) {
+	var actual = new Lexer(testcase, "LEX_TEST_BOOL_EXPR").tokenize();
 
-    // 	if (given.size() != wanted.size()) return false;
+	var expected = new ArrayList() {{
+	    add(TokenType.LPAREN);
+	    add(TokenType.LPAREN);
+	    add(TokenType.NOT);
+	    add(TokenType.IDEN);
+	    add(TokenType.AND);
+	    add(TokenType.IDEN);
+	    add(TokenType.RPAREN);
+	    add(TokenType.OR);
+	    add(TokenType.LPAREN);
+	    add(TokenType.IDEN);
+	    add(TokenType.AND);
+	    add(TokenType.NOT);
+	    add(TokenType.IDEN);
+	    add(TokenType.RPAREN);
+	    add(TokenType.RPAREN);
+	}};
+	assertListEqual(actual, expected);
+    }
 
-    // 	for (int i = 0; i < wanted.size(); i++) {
-    // 	    if (given.get(i).getType() != wanted.get(i).getType()) return false;
-    // 	}
-    // 	return true;
-    // }
+    @Test
+    public void lexWhileLoop() {
+
+	String testcase = "solange foo > 10 {}";
+	var actual = new Lexer(testcase, "LEX_TEST_WHILE_LOOP").tokenize();
+
+	var expected = new ArrayList() {{
+	    add(TokenType.WHILE);
+	    add(TokenType.IDEN);
+	    add(TokenType.GREATER);
+	    add(TokenType.NUMBERLITERAL);
+	    add(TokenType.STARTBLOCK);
+	    add(TokenType.ENDBLOCK);
+	}};
+	assertListEqual(actual, expected);
+    }
+
+    @Test
+    public void lexeImport() {
+
+	String testcase = """
+	    #benutze [
+		"Basis",
+		"Fmt",
+		      ]""";
+
+	var actual = new Lexer(testcase, "LEX_IMPORT").tokenize();
+
+	var expected = new ArrayList() {{
+	    add(TokenType.IMPORT);
+	    add(TokenType.LBRACKET);
+	    add(TokenType.STRINGLITERAL);
+	    add(TokenType.COMMA);
+	    add(TokenType.STRINGLITERAL);
+	    add(TokenType.COMMA);
+	    add(TokenType.RBRACKET);
+	}};
+	assertListEqual(actual, expected);
+    }
+
+    @Test
+    public void lexExampleFunction() {
+
+	    String testcase = """
+		fun test(x: Bool) -> Text {
+		wenn x {
+		    rueckgabe "Hello World";
+		} sonst wenn wahr {
+		    a := 20;
+		}
+
+		solange a != 10 {
+		    a = #eingabe("Gib eine Zahl ein")
+			wenn a == 10 {
+			#ausgabe("{} ist die falsche Zahl", a)
+		    }
+		}
+		rueckgabe "FOO BAR"
+	    }
+	    """;
+	    var actual = new Lexer(testcase, "LEX_TEST_EXAMPLE_FUNCTION").tokenize();
+
+	var expected = new ArrayList() {{
+	    add(TokenType.FUNCTION);
+	    add(TokenType.IDEN);
+	    add(TokenType.LPAREN);
+	    add(TokenType.IDEN);
+	    add(TokenType.COLON);
+	    add(TokenType.IDEN);
+	    add(TokenType.RPAREN);
+	    add(TokenType.ARROW);
+	    add(TokenType.IDEN);
+	    add(TokenType.STARTBLOCK);
+	    add(TokenType.IF);
+	    add(TokenType.IDEN);
+	    add(TokenType.STARTBLOCK);
+	    add(TokenType.RETURN);
+	    add(TokenType.STRINGLITERAL);
+	    add(TokenType.SEMICOLON);
+	    add(TokenType.ENDBLOCK);
+	    add(TokenType.ELSE);
+	    add(TokenType.IF);
+	    add(TokenType.TRUE);
+	    add(TokenType.STARTBLOCK);
+	    add(TokenType.IDEN);
+	    add(TokenType.VARDEF);
+	    add(TokenType.NUMBERLITERAL);
+	    add(TokenType.SEMICOLON);
+	    add(TokenType.ENDBLOCK);
+	    add(TokenType.WHILE);
+	    add(TokenType.IDEN);
+	    add(TokenType.NOTEQUAL);
+	    add(TokenType.NUMBERLITERAL);
+	    add(TokenType.STARTBLOCK);
+	    add(TokenType.IDEN);
+	    add(TokenType.EQUALSIGN);
+	    add(TokenType.READINPUT);
+	    add(TokenType.LPAREN);
+	    add(TokenType.STRINGLITERAL);
+	    add(TokenType.RPAREN);
+	    add(TokenType.IF);
+	    add(TokenType.IDEN);
+	    add(TokenType.EQUALEQUAL);
+	    add(TokenType.NUMBERLITERAL);
+	    add(TokenType.STARTBLOCK);
+	    add(TokenType.PRINT);
+	    add(TokenType.LPAREN);
+	    add(TokenType.STRINGLITERAL);
+	    add(TokenType.COMMA);
+	    add(TokenType.IDEN);
+	    add(TokenType.RPAREN);
+	    add(TokenType.ENDBLOCK);
+	    add(TokenType.ENDBLOCK);
+	    add(TokenType.RETURN);
+	    add(TokenType.STRINGLITERAL);
+	    add(TokenType.ENDBLOCK);
+	}};
+    assertListEqual(actual, expected);
+}
+
+
+    @Test
+    public void lexVarDef() {
+
+	String testcase = """
+	    foo := "Hello" + "World"
+	    """;
+	var actual = new Lexer(testcase, "LEX_TEST_VAR_DEF").tokenize();
+
+	var expected = new ArrayList() {{
+	    add(TokenType.IDEN);
+	    add(TokenType.VARDEF);
+	    add(TokenType.STRINGLITERAL);
+	    add(TokenType.PLUS);
+	    add(TokenType.STRINGLITERAL);
+	}};
+	assertListEqual(actual, expected);
+    }
+
+    @Test
+    public void lexTypeDecl() {
+
+	String testcase = """
+	    Typ: Adress {
+	    FirstName: Text,
+	    LastName: Text,
+	    Street: Text,
+	    HouseNumer: Zahl,
+	}
+	""";
+	var actual = new Lexer(testcase, "LEX_TEST_TYPEDECL").tokenize();
+
+
+	var expected = new ArrayList() {{
+	    add(TokenType.CLASS);
+	    add(TokenType.COLON);
+	    add(TokenType.IDEN);
+	    add(TokenType.STARTBLOCK);
+	    add(TokenType.IDEN);
+	    add(TokenType.COLON);
+	    add(TokenType.IDEN);
+	    add(TokenType.COMMA);
+	    add(TokenType.IDEN);
+	    add(TokenType.COLON);
+	    add(TokenType.IDEN);
+	    add(TokenType.COMMA);
+	    add(TokenType.IDEN);
+	    add(TokenType.COLON);
+	    add(TokenType.IDEN);
+	    add(TokenType.COMMA);
+	    add(TokenType.IDEN);
+	    add(TokenType.COLON);
+	    add(TokenType.IDEN);
+	    add(TokenType.COMMA);
+	    add(TokenType.ENDBLOCK);
+	}};
+	assertListEqual(actual, expected);
+    }
+
+    @Test
+    public void lexAssingment() {
+
+	String testcase = "foo = (a + 3)*3";
+	var actual = new Lexer(testcase, "LEX_TEST_ASSIGNMENT").tokenize();
+
+	var expected = new ArrayList() {{
+	    add(TokenType.IDEN);
+	    add(TokenType.EQUALSIGN);
+	    add(TokenType.LPAREN);
+	    add(TokenType.IDEN);
+	    add(TokenType.PLUS);
+	    add(TokenType.NUMBERLITERAL);
+	    add(TokenType.RPAREN);
+	    add(TokenType.MULTIPLY);
+	    add(TokenType.NUMBERLITERAL);
+	}};
+	assertListEqual(actual, expected);
+    }
+    @Test
+    public void lexForLoop() {
+
+	String testcase = "fuer i := 0 bis 10 {}";
+
+	var actual = new Lexer(testcase, "LEX_TEST_FOR_LOOP").tokenize();
+
+	var expected = new ArrayList() {{
+	    add(TokenType.FOR);
+	    add(TokenType.IDEN);
+	    add(TokenType.VARDEF);
+	    add(TokenType.NUMBERLITERAL);
+	    add(TokenType.UNTIL);
+	    add(TokenType.NUMBERLITERAL);
+	    add(TokenType.STARTBLOCK);
+	    add(TokenType.ENDBLOCK);
+	}};
+	assertListEqual(actual, expected);
+    }
+
+    public static void assertListEqual(List<Token> actual, List<TokenType> expected) {
+	assertTrue("length of expected != length of actual", expected.size() == actual.size());
+	var res = Streams.zip(expected.stream(), actual.stream(), (a, e) -> e.getType() != a)
+	    .anyMatch(x -> x);
+	assertFalse("expected TokenTypes != actual TokenTypes", res);
+    }
+public static void diffPrintList(List<Token> actual, List<TokenType> expected) {
+    for (int i = 0; i < actual.size(); i++) {
+	if (actual.get(i).getType() != expected.get(i)) {
+	    System.out.printf("[DIFF] :: actual token with type %s at index %d, does not mactch expected type %s %n", actual.get(i).getType(), i, expected.get(i));
+	}
+    }
+    }
 }
