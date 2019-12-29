d37c6b18670f313c130af2b4913ec990ac1eca80
Simon Kunz <simon.kunz21@gmail.com>
2019-12-29 12:43:27 +0100

    refactor peek method

--- src/main/java/util/Iter.java
+++ src/main/java/util/Iter.java
@@ -32,47 +32,47 @@ public class Iter<T> implements Iterator {
     // peeks looks at the item directly under the cursor
     // which means peeking one item into the future
     public T peek() {
-	if (!this.hasNext()) return null;
-	return buffer[cursor];
+	if (this.hasNext()) return buffer[cursor];
+	return null;
     }
 
-    public T peekNext() {
-	if (cursor +1 > buffer.length) {
-	    return null;
+	public T peekNext() {
+	    if (cursor +1 > buffer.length) {
+		return null;
+	    }
+	    return buffer[cursor +1];
 	}
-	return buffer[cursor +1];
-    }
 
-    public void setBackOnePosition() {
-	cursor -= 1;
-    }
+	public void setBackOnePosition() {
+	    cursor -= 1;
+	}
 
-    public void remove() {
-	throw new UnsupportedOperationException();
-    }
+	public void remove() {
+	    throw new UnsupportedOperationException();
+	}
 
-    public Stream<T> stream() {
-	return Stream.of(buffer);
-    }
+	public Stream<T> stream() {
+	    return Stream.of(buffer);
+	}
 
-    public T[] getBuffer() {
-	return this.buffer;
-    }
+	public T[] getBuffer() {
+	    return this.buffer;
+	}
 
-    public void setBuffer(T[] buffer) {
-	this.buffer = buffer;
-    }
+	public void setBuffer(T[] buffer) {
+	    this.buffer = buffer;
+	}
 
-    public int getCursor() {
-	return this.cursor;
-    }
+	public int getCursor() {
+	    return this.cursor;
+	}
 
-    @Override
-    public String toString() {
-	return new GsonBuilder()
-	    .setPrettyPrinting()
-	    .serializeNulls()
-	    .create()
-	    .toJson(this);
-    }
+	@Override
+	    public String toString() {
+	    return new GsonBuilder()
+		.setPrettyPrinting()
+		.serializeNulls()
+		.create()
+		.toJson(this);
+	}
 }
