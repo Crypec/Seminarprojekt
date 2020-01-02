7a1fcaf349186081a7560aafe6c98e43b75d3a56
Simon Kunz <simon.kunz21@gmail.com>
2020-01-02 15:55:45 +0100

    add interpreter

---
+++ src/main/java/core/Interp.java
@@ -0,0 +1,33 @@
+package core;
+
+// import util.*;
+
+public class Interp {
+
+    //     public Object eval(Expr ASTNode) {
+// 	return ASTNode.accept(this);
+//     }
+
+
+//     @Override
+//     public Object visitAssignExpr(Expr.Assign ASTNode) {
+// 	throw new UnsupportedOperationException();
+//     }
+
+//     @Override
+//     public Object visitInputExpr(Expr.Input ASTNode) {
+// 	throw new UnsupportedOperationException();
+//     }
+
+//     @Override
+//     public Object visitVariableExpr(Expr.Variable ASTNode) {
+// 	throw new UnsupportedOperationException();
+//     }
+
+//     @Override
+//     public Object visitUnaryExpr(Expr.Unary ASTNode) {
+// 	return switch (ASTNode.getOperator().getType()) {
+// 	case MINUS: yield -ASTNode.accept(this);
+// 	}
+//     }
+}
