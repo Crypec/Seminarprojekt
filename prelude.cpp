cc9daaaa14dae16479100eca29d3bb64a3721b2d
Simon Kunz <simon.kunz21@gmail.com>
2020-01-17 14:15:01 +0100

    add cpp prelude

---
+++ prelude.cpp
@@ -0,0 +1,14 @@
+#include <fmt/format.h>
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <nlohmann/json.hpp>
+
+using nlohmann::json;
+
+std::string __internal_get_input(std::string message) {
+  fmt::print("{}", message);
+  std::string input;
+  std::getline(std::cin, input);
+  return input;
+}
