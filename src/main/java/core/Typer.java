653ca0b14dd48c3c08bd8d4739ac05853f51db13
Simon Kunz <simon.kunz21@gmail.com>
2020-01-17 14:04:03 +0100

    add typer to infer types to AST

--- src/main/java/core/Typer.java
+++ src/main/java/core/Typer.java
@@ -1,11 +1,114 @@
 package core;
 
 import util.*;
+import lombok.*;
+import java.util.*;
+import java.text.*;
 
-public class Typer {
+public class Typer implements Expr.Visitor<Void> {
+
+    public void infer(Expr ASTNode) {
+	ASTNode.accept(this);
+    }
+
+    @Override
+    public Void visitArrayAccessExpr(Expr.ArrayAccess expr) {
+	return null;
+    }
     
-    //TODO(Simon): Perform typechecking :D
-    public static boolean isValid(Stmt AST) {
-	return true;
+    @Override
+    public Void visitStructLiteralExpr(Expr.StructLiteral expr) {
+	return null;
+    }
+
+    @Override
+    public Void visitAssignExpr(Expr.Assign expr) {
+	return null;
+    }
+
+    @Override
+    public Void visitInputExpr(Expr.Input expr) {
+	expr.setType(new TypeInfo(TypeInfo.STRINGTYPE));
+	return null;
+    }
+
+    @Override
+    public Void visitVariableExpr(Expr.Variable expr) {
+	return null;
+    }
+
+    @Override
+    public Void visitUnaryExpr(Expr.Unary expr) {
+	expr.getRight().accept(this);
+	return null;
+    }
+
+    @Override
+    public Void visitSelfExpr(Expr.Self expr) {
+	return null;
+    }
+
+    @Override
+    public Void visitSetExpr(Expr.Set expr) {
+	return null;
+    }
+    
+    @Override
+    public Void visitGetExpr(Expr.Get expr) {
+	return null;
+    }
+
+    @Override
+    public Void visitLiteralExpr(Expr.Literal expr) {
+	if (expr.getValue() instanceof Double) {
+	    expr.setType(new TypeInfo(TypeInfo.NUMBERTYPE));
+	} else if (expr.getValue() instanceof String) {
+	    expr.setType(new TypeInfo(TypeInfo.STRINGTYPE));
+	} else if (expr.getValue() instanceof Boolean) {
+	    expr.setType(new TypeInfo(TypeInfo.BOOLEANTYPE));
+	} else {
+	    System.out.println("Internal Error you should never see this :: type error for literal");
+	}
+	return null;
+    }
+
+    @Override
+    public Void visitGroupingExpr(Expr.Grouping expr) {
+	expr.getExpression().accept(this);
+	return null;
+    }
+
+    @Override
+    public Void visitCallExpr(Expr.Call expr) {
+	return null;
+    }
+
+    @Override
+    public Void visitBinaryExpr(Expr.Binary expr) {
+
+	expr.getLeft().accept(this);
+	expr.getRight().accept(this);
+	
+	var err = Report.builder()
+	    .wasFatal(true)
+	    .errType("Typenfehler")
+	    .errMsg("Kann folgende Operation nicht auf folgende Typen ausfuehren!")
+	    .token(null)
+	    .url(null)
+	    .build();
+	
+	boolean failed = false;
+	
+	// if (right.getType().getTypeTag() != left.getTypeInfo().getTypeTag()) {
+	//     err.setToken(expr.getOperator());
+	//     failed = true;
+	// } else if (!TypeInfoTag.isAllowed(right.getTypeInfo(), expr.getOperator())) {
+	//     System.out.println(err);
+	//     failed = true;
+	// }
+
+	// if (failed) return null;
+	// expr.setTypeInfo(right.getTypeInfo());
+	return null;
     }
 }
