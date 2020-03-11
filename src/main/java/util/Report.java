6e780fbc899c53c35e4b292700b93d496fa2bd21
Simon Kunz <simon.kunz21@gmail.com>
2020-03-11 16:56:02 +0100

    format source code

--- src/main/java/util/Report.java
+++ src/main/java/util/Report.java
@@ -10,89 +10,91 @@ import lombok.*;
 @EqualsAndHashCode
 public class Report {
 
-    // TODO(Simon): rename to somehting more generic, we use the same excpetion in the typechecking
-    // stage as well
-    public class Error extends RuntimeException {}
+  // TODO(Simon): rename to somehting more generic, we use the same excpetion in the typechecking
+  // stage as well
+  public class Error extends RuntimeException {}
 
-    // if we encounter a fatal error while parsing we set this field to not execute corrupted code
-    public static boolean hadErr = false;
+  // if we encounter a fatal error while parsing we set this field to not execute corrupted code
+  public static boolean hadErr = false;
 
+  private boolean wasFatal = false;
+
+  private String errType;
+  private String errMsg;
+
+  private List<String> examples = new ArrayList();
+
+  private Token token;
+  private String url;
+
+  public static ReportBuilder builder() {
+    return new ReportBuilder();
+  }
+
+  @NoArgsConstructor
+  public static class ReportBuilder {
     private boolean wasFatal = false;
 
     private String errType;
     private String errMsg;
 
-    private List<String> examples = new ArrayList();
+    public List<String> examples = new ArrayList();
 
     private Token token;
     private String url;
 
-    public static ReportBuilder builder() {
-        return new ReportBuilder();
+    public ReportBuilder example(String example) {
+      this.examples.add(example);
+      return this;
     }
 
-    @NoArgsConstructor
-    public static class ReportBuilder {
-
-        private boolean wasFatal = false;
-
-        private String errType;
-        private String errMsg;
-
-        public List<String> examples = new ArrayList();
-
-        private Token token;
-        private String url;
-
-        public ReportBuilder example(String example) {
-            this.examples.add(example);
-            return this;
-        }
-
-        public ReportBuilder errType(String errType) {
-            this.errType = errType;
-            return this;
-        }
-
-        public ReportBuilder errMsg(String errMsg) {
-            this.errMsg = errMsg;
-            return this;
-        }
-
-        public ReportBuilder wasFatal(boolean wasFatal) {
-            this.wasFatal = wasFatal;
-            return this;
-        }
-
-        public ReportBuilder token(Token token) {
-            this.token = token;
-            return this;
-        }
-
-        public ReportBuilder url(String url) {
-            this.url = url;
-            return this;
-        }
-
-        public Report build() {
-            return new Report(wasFatal, errType, errMsg, examples, token, url);
-        }
+    public ReportBuilder errType(String errType) {
+      this.errType = errType;
+      return this;
     }
 
-    public void sync() {
-        if (wasFatal) {
-            Report.hadErr = true;
-            throw new Report.Error();
-        }
+    public ReportBuilder errMsg(String errMsg) {
+      this.errMsg = errMsg;
+      return this;
     }
 
-    public Report print() {
-        System.err.println(this);
-		return this;
-	}
-
-	@Override
-	public String toString() {
-        return new GsonBuilder().setPrettyPrinting().create().toJson(this);
+    public ReportBuilder wasFatal(boolean wasFatal) {
+      this.wasFatal = wasFatal;
+      return this;
     }
+
+    public ReportBuilder token(Token token) {
+      this.token = token;
+      return this;
+    }
+
+    public ReportBuilder url(String url) {
+      this.url = url;
+      return this;
+    }
+
+    public Report build() {
+      return new Report(wasFatal, errType, errMsg, examples, token, url);
+    }
+  }
+
+  public void sync() {
+    Report.hadErr |= this.wasFatal;
+    throw new Report.Error();
+  }
+
+  public Report print() {
+	  Report.hadErr |= this.wasFatal;
+	  System.err.println(this);
+	  return this;
+  }
+
+  @Override
+  public String toString() {
+    return new GsonBuilder()
+      .setPrettyPrinting()
+      .disableHtmlEscaping()
+      .create()
+      .toJson(this);
+  }
 }
