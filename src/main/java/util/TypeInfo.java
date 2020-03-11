c0892526050f5abed7dbccf02993944833689f3d
Simon Kunz <simon.kunz21@gmail.com>
2020-03-11 16:55:29 +0100

    move to new representation of basic language types

--- src/main/java/util/TypeInfo.java
+++ src/main/java/util/TypeInfo.java
@@ -1,96 +1,119 @@
 package util;
 
-import com.google.gson.*;
+import java.util.*;
+import java.util.stream.*;
 import lombok.*;
+import lombok.experimental.*;
+import java.io.Serializable;
 
 @Getter
 @Setter
-@Builder
-@AllArgsConstructor
-public class TypeInfo {
+@SuperBuilder
+public abstract class TypeInfo implements Serializable {
 
-  public static final String NUMBERTYPE = "Zahl";
-  public static final String BOOLEANTYPE = "Bool";
-  public static final String STRINGTYPE = "Text";
+	public static final String NUMBERTYPE = "Zahl";
+	public static final String BOOLEANTYPE = "Bool";
+	public static final String STRINGTYPE = "Text";
+	public static final String VOIDTYPE = "()";
 
-  private String typeString;
-  private Token location;
-  /**
-     If a subtreee contains a type marked as dirty, the whole expression tree is
-     invalid.
+	@EqualsAndHashCode.Exclude private Token location;
 
-     Example:
-     a := 2 + wahr; // this expression is not valid because we cant add a number
-     with an boolean value in this language. b := a + 2; // because the whole
-     expression tree of a is dirty, we cant proceed to validate b.
+	@EqualsAndHashCode(callSuper=false)
+	@Getter
+	@SuperBuilder
+	public static class Array extends TypeInfo {
+		private TypeInfo elementType;
 
-     The isDirty value has therefore to be  propagated through the whole
-     expression tree. And we cant just mark one type interaction as dirty.
-  */
-  private boolean isDirty;
+		@Override
+		public String getBaseTypeString() {
+			return elementType.getBaseTypeString();
+		}
 
-  /**
-   * Describes if the type is nested in an array. Example: a: foo = ...; here a
-   * is of type foo and not contained in an array, therefore the arraylevel
-   * would be 0. Example: a: [foo] = ...; here a is of type array of foo and the
-   * arraylevel 1. a: [[foo]] = ...; a is of type of array of array of foo and
-   * the arraylevel would be 2;
-   */
-  private int arrayLevel;
+		@Override
+		public String toString() {
+			return "[" + elementType.toString() + "]";
+		}
+	}
 
-  public static boolean isAllowed(TypeInfo typeInfo, Token operator) {
-    if (typeInfo.arrayLevel != 0) {
-      return false;
-    }
-    return switch (typeInfo.typeString) {
-    case NUMBERTYPE:
-      yield switch (operator.getType()) {
-      case PLUS, MINUS, MULTIPLY, DIVIDE:
-        yield true;
-      default:
-        yield false;
-      };
-    case BOOLEANTYPE:
-      yield switch (operator.getType()) {
-      case AND, OR, NOT:
-        yield true;
-      default:
-        yield false;
-      };
-    case STRINGTYPE:
-      yield switch (operator.getType()) {
-      case PLUS: {
-                    Report.builder()
-						.wasFatal(true)
-						.errType("TypenFehler")
-						.errMsg("In dieser Sprache duerfen Zeichenfolgen nicht mit dem + Operator anneinanderehaengt werden, du solltest dafuer die 'format' Funktion nutzen!")
-						.example("""
-							 foo: Text = format("Hello {}", "World");
-							 """)
-						.example("""
-							 gruss := format("Hallo {}, wie geht es dir heute?", name);
-							 """)
-						.example("""
-							 gedreht := format("{0} {1}", "Welt :D" , "Hallo");
-							 """)
-						.build()
-						.print();
+	@Getter
+	@EqualsAndHashCode(callSuper=false)
+	@SuperBuilder
+	public static class Primitive extends TypeInfo {
+		private String typeString;
+
+		@Override
+		public String getBaseTypeString() {
+			return this.typeString;
+		}
+
+		@Override 
+		public String toString() {
+			return this.typeString;
+		}
+	}
+
+	@Getter
+	@EqualsAndHashCode(callSuper=false)
+	@SuperBuilder
+	public static class Tuple extends TypeInfo {
+		private ArrayList<TypeInfo> elementTypes;
+		@Override
+		public String getBaseTypeString() {
+			throw new UnsupportedOperationException(
+													"A tuple consists of more than 1 type and does therefor not have a single basetypestring!");
+		}
+		@Override
+		public String toString() {
+			return "(" + elementTypes.stream().map(e -> e.toString()).collect(Collectors.joining(",")) + ")";
+		}
+	} 
+
+	public static TypeInfo.Primitive voidType(Token t) {
+		return TypeInfo.Primitive.builder().typeString(TypeInfo.VOIDTYPE).location(t).build();
+	}
+
+	public abstract String getBaseTypeString();
+
+	public static boolean isAllowed(TypeInfo type, Token operator) {
+
+		if (!(type instanceof TypeInfo.Primitive)) return false;
+		TypeInfo.Primitive cmp = (TypeInfo.Primitive) type;
+		return switch (cmp.typeString) {
+		case NUMBERTYPE:
+			yield switch (operator.getType()) {
+			case PLUS, MINUS, MULTIPLY, DIVIDE, EQUALEQUAL, NOTEQUAL, LESS, LESSEQUAL, GREATER, GREATEREQUAL:
+				yield true;
+			default:
 				yield false;
-      }
-      default:
-        yield false; // use format function to concat strings
-      };
-    default:
-      yield false;
-    };
-  }
-
-  public void setIsDirty(boolean isDirty) { this.isDirty = isDirty; }
-
-  public boolean getIsDirty() { return this.isDirty; }
-
-  @Override
-  public String toString() {
-		return "[".repeat(arrayLevel) + typeString + "]".repeat(arrayLevel);
-  }
+			};
+		case BOOLEANTYPE:
+			yield switch (operator.getType()) {
+			case AND, OR, NOT, EQUALEQUAL, NOTEQUAL:
+				yield true;
+			default:
+				yield false;
+			};
+		case STRINGTYPE:
+			yield switch (operator.getType()) {
+			case EQUALEQUAL, NOTEQUAL:
+				yield true;
+			case PLUS: {
+				Report.builder().wasFatal(true).errType("TypenFehler").errMsg(
+						"In dieser Sprache duerfen Zeichenfolgen nicht mit dem + Operator anneinanderehaengt werden, du solltest dafuer die 'format' Funktion nutzen!")
+					.example("""
+							 foo: Text = format("Hello {}", "World");
+							 """).example("""
+							 gruss := format("Hallo {}, wie geht es dir heute?", name);
+							 """).example("""
+								gedreht := format("{0} {1}", "Welt :D" , "Hallo");
+							 """).build().print();
+				yield false;
+			}
+			default:
+				yield false; // use format function to concat strings
+			};
+		default:
+			yield false;
+		};
+	}
 }
